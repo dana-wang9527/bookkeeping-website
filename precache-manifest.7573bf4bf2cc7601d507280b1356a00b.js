ffc923d1b907413bcaf65c5f235663e1b82fe8e7
wangdana <wangdana1998@gmail.com>
2021-03-14 11:34:57 +0800

    deploy

---
+++ precache-manifest.7573bf4bf2cc7601d507280b1356a00b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b161ccca52634f2d0fcf",
+    "url": "/bookkeeping-website/css/app.390d5494.css"
+  },
+  {
+    "revision": "110d53b1f3e6ec1d119b98d559c58401",
+    "url": "/bookkeeping-website/index.html"
+  },
+  {
+    "revision": "b161ccca52634f2d0fcf",
+    "url": "/bookkeeping-website/js/app.60a34f6d.js"
+  },
+  {
+    "revision": "08de0916eb6f62166e7f",
+    "url": "/bookkeeping-website/js/chunk-vendors.a9430957.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/bookkeeping-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bookkeeping-website/robots.txt"
+  }
+]);
